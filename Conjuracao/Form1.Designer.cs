f1601df5e6092e788303099fb69abf08e157ba55
Luiz Brito <luizrickrg14@gmail.com>
2021-06-04 14:41:29 -0300

    Quinto Envio

--- Conjuracao/Form1.Designer.cs
+++ Conjuracao/Form1.Designer.cs
@@ -31,7 +31,7 @@ namespace Conjuracao
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Thunderstorm));
             this.dado1picbox = new System.Windows.Forms.PictureBox();
-            this.Button = new System.Windows.Forms.Button();
+            this.buttonDados = new System.Windows.Forms.Button();
             this.dado2picbox = new System.Windows.Forms.PictureBox();
             this.dado3picbox = new System.Windows.Forms.PictureBox();
             this.dado4picbox = new System.Windows.Forms.PictureBox();
@@ -41,39 +41,55 @@ namespace Conjuracao
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.progressBar3 = new System.Windows.Forms.ProgressBar();
             this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.groupBoxPartida = new System.Windows.Forms.GroupBox();
+            this.groupBoxMenu = new System.Windows.Forms.GroupBox();
+            this.textBoxEntraJogador = new System.Windows.Forms.TextBox();
+            this.buttonJogar = new System.Windows.Forms.Button();
+            this.buttonAdicionar = new System.Windows.Forms.Button();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.progressBar6 = new System.Windows.Forms.ProgressBar();
+            this.progressBar7 = new System.Windows.Forms.ProgressBar();
+            this.progressBar8 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.dado1picbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dado2picbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dado3picbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dado4picbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dado5picbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dado6picbox)).BeginInit();
+            this.groupBoxPartida.SuspendLayout();
+            this.groupBoxMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // dado1picbox
             // 
+            this.dado1picbox.AccessibleName = "Jogo";
+            this.dado1picbox.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.dado1picbox.Image = ((System.Drawing.Image)(resources.GetObject("dado1picbox.Image")));
             this.dado1picbox.InitialImage = null;
-            this.dado1picbox.Location = new System.Drawing.Point(116, 53);
+            this.dado1picbox.Location = new System.Drawing.Point(40, 49);
             this.dado1picbox.Name = "dado1picbox";
             this.dado1picbox.Size = new System.Drawing.Size(75, 75);
             this.dado1picbox.TabIndex = 0;
             this.dado1picbox.TabStop = false;
             // 
-            // Button
+            // buttonDados
             // 
-            this.Button.Location = new System.Drawing.Point(161, 226);
-            this.Button.Name = "Button";
-            this.Button.Size = new System.Drawing.Size(146, 31);
-            this.Button.TabIndex = 1;
-            this.Button.Text = "COMEÇAR";
-            this.Button.UseVisualStyleBackColor = true;
-            this.Button.Click += new System.EventHandler(this.Button_Click);
+            this.buttonDados.AccessibleName = "Jogo";
+            this.buttonDados.Location = new System.Drawing.Point(83, 222);
+            this.buttonDados.Name = "buttonDados";
+            this.buttonDados.Size = new System.Drawing.Size(146, 31);
+            this.buttonDados.TabIndex = 1;
+            this.buttonDados.Text = "COMEÇAR";
+            this.buttonDados.UseVisualStyleBackColor = true;
+            this.buttonDados.Click += new System.EventHandler(this.buttonDados_Click);
             // 
             // dado2picbox
             // 
+            this.dado2picbox.AccessibleName = "Jogo";
+            this.dado2picbox.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.dado2picbox.Image = ((System.Drawing.Image)(resources.GetObject("dado2picbox.Image")));
             this.dado2picbox.InitialImage = null;
-            this.dado2picbox.Location = new System.Drawing.Point(197, 53);
+            this.dado2picbox.Location = new System.Drawing.Point(121, 49);
             this.dado2picbox.Name = "dado2picbox";
             this.dado2picbox.Size = new System.Drawing.Size(75, 75);
             this.dado2picbox.TabIndex = 2;
@@ -81,9 +97,11 @@ namespace Conjuracao
             // 
             // dado3picbox
             // 
+            this.dado3picbox.AccessibleName = "Jogo";
+            this.dado3picbox.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.dado3picbox.Image = ((System.Drawing.Image)(resources.GetObject("dado3picbox.Image")));
             this.dado3picbox.InitialImage = null;
-            this.dado3picbox.Location = new System.Drawing.Point(278, 53);
+            this.dado3picbox.Location = new System.Drawing.Point(202, 49);
             this.dado3picbox.Name = "dado3picbox";
             this.dado3picbox.Size = new System.Drawing.Size(75, 75);
             this.dado3picbox.TabIndex = 3;
@@ -91,9 +109,11 @@ namespace Conjuracao
             // 
             // dado4picbox
             // 
+            this.dado4picbox.AccessibleName = "Jogo";
+            this.dado4picbox.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.dado4picbox.Image = ((System.Drawing.Image)(resources.GetObject("dado4picbox.Image")));
             this.dado4picbox.InitialImage = null;
-            this.dado4picbox.Location = new System.Drawing.Point(116, 134);
+            this.dado4picbox.Location = new System.Drawing.Point(40, 130);
             this.dado4picbox.Name = "dado4picbox";
             this.dado4picbox.Size = new System.Drawing.Size(75, 75);
             this.dado4picbox.TabIndex = 4;
@@ -101,9 +121,11 @@ namespace Conjuracao
             // 
             // dado5picbox
             // 
+            this.dado5picbox.AccessibleName = "Jogo";
+            this.dado5picbox.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.dado5picbox.Image = ((System.Drawing.Image)(resources.GetObject("dado5picbox.Image")));
             this.dado5picbox.InitialImage = null;
-            this.dado5picbox.Location = new System.Drawing.Point(197, 134);
+            this.dado5picbox.Location = new System.Drawing.Point(121, 130);
             this.dado5picbox.Name = "dado5picbox";
             this.dado5picbox.Size = new System.Drawing.Size(75, 75);
             this.dado5picbox.TabIndex = 5;
@@ -111,9 +133,11 @@ namespace Conjuracao
             // 
             // dado6picbox
             // 
+            this.dado6picbox.AccessibleName = "Jogo";
+            this.dado6picbox.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.dado6picbox.Image = ((System.Drawing.Image)(resources.GetObject("dado6picbox.Image")));
             this.dado6picbox.InitialImage = null;
-            this.dado6picbox.Location = new System.Drawing.Point(278, 134);
+            this.dado6picbox.Location = new System.Drawing.Point(202, 130);
             this.dado6picbox.Name = "dado6picbox";
             this.dado6picbox.Size = new System.Drawing.Size(75, 75);
             this.dado6picbox.TabIndex = 6;
@@ -121,53 +145,176 @@ namespace Conjuracao
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(402, 76);
+            this.progressBar1.AccessibleName = "Jogo";
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(334, 63);
             this.progressBar1.Maximum = 6;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(100, 23);
             this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
             // 
             // progressBar2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(402, 105);
+            this.progressBar2.AccessibleName = "Jogo";
+            this.progressBar2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar2.Enabled = false;
+            this.progressBar2.Location = new System.Drawing.Point(334, 101);
             this.progressBar2.Maximum = 6;
             this.progressBar2.Name = "progressBar2";
             this.progressBar2.Size = new System.Drawing.Size(100, 23);
             this.progressBar2.TabIndex = 8;
+            this.progressBar2.Visible = false;
             // 
             // progressBar3
             // 
-            this.progressBar3.Location = new System.Drawing.Point(402, 134);
+            this.progressBar3.AccessibleName = "Jogo";
+            this.progressBar3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar3.Enabled = false;
+            this.progressBar3.Location = new System.Drawing.Point(334, 141);
             this.progressBar3.Maximum = 6;
             this.progressBar3.Name = "progressBar3";
             this.progressBar3.Size = new System.Drawing.Size(100, 23);
             this.progressBar3.TabIndex = 9;
+            this.progressBar3.Visible = false;
             // 
             // progressBar4
             // 
-            this.progressBar4.Location = new System.Drawing.Point(402, 163);
+            this.progressBar4.AccessibleName = "Jogo";
+            this.progressBar4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar4.Enabled = false;
+            this.progressBar4.Location = new System.Drawing.Point(334, 182);
             this.progressBar4.Maximum = 6;
             this.progressBar4.Name = "progressBar4";
             this.progressBar4.Size = new System.Drawing.Size(100, 23);
             this.progressBar4.TabIndex = 10;
+            this.progressBar4.Visible = false;
+            // 
+            // groupBoxPartida
+            // 
+            this.groupBoxPartida.Controls.Add(this.progressBar5);
+            this.groupBoxPartida.Controls.Add(this.progressBar6);
+            this.groupBoxPartida.Controls.Add(this.progressBar7);
+            this.groupBoxPartida.Controls.Add(this.progressBar8);
+            this.groupBoxPartida.Controls.Add(this.dado3picbox);
+            this.groupBoxPartida.Controls.Add(this.progressBar4);
+            this.groupBoxPartida.Controls.Add(this.dado1picbox);
+            this.groupBoxPartida.Controls.Add(this.progressBar3);
+            this.groupBoxPartida.Controls.Add(this.buttonDados);
+            this.groupBoxPartida.Controls.Add(this.progressBar2);
+            this.groupBoxPartida.Controls.Add(this.dado2picbox);
+            this.groupBoxPartida.Controls.Add(this.progressBar1);
+            this.groupBoxPartida.Controls.Add(this.dado4picbox);
+            this.groupBoxPartida.Controls.Add(this.dado6picbox);
+            this.groupBoxPartida.Controls.Add(this.dado5picbox);
+            this.groupBoxPartida.Enabled = false;
+            this.groupBoxPartida.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxPartida.Name = "groupBoxPartida";
+            this.groupBoxPartida.Size = new System.Drawing.Size(600, 297);
+            this.groupBoxPartida.TabIndex = 11;
+            this.groupBoxPartida.TabStop = false;
+            this.groupBoxPartida.Text = "Partida";
+            this.groupBoxPartida.Visible = false;
+            // 
+            // groupBoxMenu
+            // 
+            this.groupBoxMenu.Controls.Add(this.textBoxEntraJogador);
+            this.groupBoxMenu.Controls.Add(this.buttonJogar);
+            this.groupBoxMenu.Controls.Add(this.buttonAdicionar);
+            this.groupBoxMenu.Location = new System.Drawing.Point(12, 315);
+            this.groupBoxMenu.Name = "groupBoxMenu";
+            this.groupBoxMenu.Size = new System.Drawing.Size(600, 297);
+            this.groupBoxMenu.TabIndex = 11;
+            this.groupBoxMenu.TabStop = false;
+            this.groupBoxMenu.Text = "Menu";
+            // 
+            // textBoxEntraJogador
+            // 
+            this.textBoxEntraJogador.Location = new System.Drawing.Point(189, 185);
+            this.textBoxEntraJogador.Name = "textBoxEntraJogador";
+            this.textBoxEntraJogador.Size = new System.Drawing.Size(222, 20);
+            this.textBoxEntraJogador.TabIndex = 3;
+            this.textBoxEntraJogador.TextChanged += new System.EventHandler(this.textBoxEntraJogador_TextChanged);
+            // 
+            // buttonJogar
+            // 
+            this.buttonJogar.Enabled = false;
+            this.buttonJogar.Location = new System.Drawing.Point(303, 211);
+            this.buttonJogar.Name = "buttonJogar";
+            this.buttonJogar.Size = new System.Drawing.Size(108, 23);
+            this.buttonJogar.TabIndex = 1;
+            this.buttonJogar.Text = "JOGAR";
+            this.buttonJogar.UseVisualStyleBackColor = true;
+            this.buttonJogar.Click += new System.EventHandler(this.buttonJogar_Click);
+            // 
+            // buttonAdicionar
+            // 
+            this.buttonAdicionar.Enabled = false;
+            this.buttonAdicionar.Location = new System.Drawing.Point(189, 211);
+            this.buttonAdicionar.Name = "buttonAdicionar";
+            this.buttonAdicionar.Size = new System.Drawing.Size(108, 23);
+            this.buttonAdicionar.TabIndex = 0;
+            this.buttonAdicionar.Text = "Adicionar";
+            this.buttonAdicionar.UseVisualStyleBackColor = true;
+            this.buttonAdicionar.Click += new System.EventHandler(this.buttonAdicionar_Click);
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.AccessibleName = "Jogo";
+            this.progressBar5.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar5.Enabled = false;
+            this.progressBar5.Location = new System.Drawing.Point(473, 63);
+            this.progressBar5.Maximum = 6;
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(100, 23);
+            this.progressBar5.TabIndex = 14;
+            this.progressBar5.Visible = false;
+            // 
+            // progressBar6
+            // 
+            this.progressBar6.AccessibleName = "Jogo";
+            this.progressBar6.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar6.Enabled = false;
+            this.progressBar6.Location = new System.Drawing.Point(473, 101);
+            this.progressBar6.Maximum = 6;
+            this.progressBar6.Name = "progressBar6";
+            this.progressBar6.Size = new System.Drawing.Size(100, 23);
+            this.progressBar6.TabIndex = 13;
+            this.progressBar6.Visible = false;
+            // 
+            // progressBar7
+            // 
+            this.progressBar7.AccessibleName = "Jogo";
+            this.progressBar7.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar7.Enabled = false;
+            this.progressBar7.Location = new System.Drawing.Point(473, 141);
+            this.progressBar7.Maximum = 6;
+            this.progressBar7.Name = "progressBar7";
+            this.progressBar7.Size = new System.Drawing.Size(100, 23);
+            this.progressBar7.TabIndex = 12;
+            this.progressBar7.Visible = false;
+            // 
+            // progressBar8
+            // 
+            this.progressBar8.AccessibleName = "Jogo";
+            this.progressBar8.BackColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar8.Enabled = false;
+            this.progressBar8.Location = new System.Drawing.Point(473, 182);
+            this.progressBar8.Maximum = 6;
+            this.progressBar8.Name = "progressBar8";
+            this.progressBar8.Size = new System.Drawing.Size(100, 23);
+            this.progressBar8.TabIndex = 11;
+            this.progressBar8.Visible = false;
             // 
             // Thunderstorm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Desktop;
-            this.ClientSize = new System.Drawing.Size(624, 321);
-            this.Controls.Add(this.progressBar4);
-            this.Controls.Add(this.progressBar3);
-            this.Controls.Add(this.progressBar2);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.dado6picbox);
-            this.Controls.Add(this.dado5picbox);
-            this.Controls.Add(this.dado4picbox);
-            this.Controls.Add(this.dado3picbox);
-            this.Controls.Add(this.dado2picbox);
-            this.Controls.Add(this.Button);
-            this.Controls.Add(this.dado1picbox);
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.groupBoxMenu);
+            this.Controls.Add(this.groupBoxPartida);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Thunderstorm";
             this.Text = "Thunderstorm";
@@ -177,12 +324,15 @@ namespace Conjuracao
             ((System.ComponentModel.ISupportInitialize)(this.dado4picbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dado5picbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dado6picbox)).EndInit();
+            this.groupBoxPartida.ResumeLayout(false);
+            this.groupBoxMenu.ResumeLayout(false);
+            this.groupBoxMenu.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.Button buttonDados;
         public System.Windows.Forms.PictureBox dado1picbox;
         public System.Windows.Forms.PictureBox dado2picbox;
         public System.Windows.Forms.PictureBox dado3picbox;
@@ -193,6 +343,15 @@ namespace Conjuracao
         private System.Windows.Forms.ProgressBar progressBar2;
         private System.Windows.Forms.ProgressBar progressBar3;
         private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.GroupBox groupBoxPartida;
+        private System.Windows.Forms.GroupBox groupBoxMenu;
+        private System.Windows.Forms.Button buttonJogar;
+        private System.Windows.Forms.Button buttonAdicionar;
+        private System.Windows.Forms.TextBox textBoxEntraJogador;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ProgressBar progressBar6;
+        private System.Windows.Forms.ProgressBar progressBar7;
+        private System.Windows.Forms.ProgressBar progressBar8;
     }
 }
 
--- Conjuracao/Form1.cs
+++ Conjuracao/Form1.cs
@@ -12,8 +12,9 @@ namespace Conjuracao
 
         List<Dado> dadosLista = new List<Dado>();
         Dado[] dados = new Dado[6];
-        ProgressBar[] barras = new ProgressBar[4];
+        ProgressBar[] barras = new ProgressBar[8];
         int posicao = 0;
+        int barrasAtivadas = 0;
 
         bool JogoRolando = false; // False - O jogo/partida não está em andamento / True - O jogo/partida está em andamento
         bool TirouAoMenosUm1 = false;
@@ -32,17 +33,21 @@ namespace Conjuracao
             barras[1] = progressBar2;
             barras[2] = progressBar3;
             barras[3] = progressBar4;
+            barras[4] = progressBar5;
+            barras[5] = progressBar6;
+            barras[6] = progressBar7;
+            barras[7] = progressBar8;
         }
 
         // Inputs
 
-        private void Button_Click(object sender, EventArgs e)
+        private void buttonDados_Click(object sender, EventArgs e)
         {
-            switch (Button.Text)
+            switch (buttonDados.Text)
             {
                 case "COMEÇAR":
-                    CriaLista(dados);
-                    Button.Text = "ROLAR";
+                    Cria_dadosLista(dados);
+                    buttonDados.Text = "ROLAR";
                     JogoRolando = true;
                     break;
 
@@ -69,22 +74,22 @@ namespace Conjuracao
                     {
                         if (JogoRolando is true)
                         {
-                            Button.Text = "REPOR";
+                            buttonDados.Text = "REPOR";
                         }
                         else
                         {
-                            Button.Enabled = false;
+                            buttonDados.Enabled = false;
                         }
                     }
 
                     if(barras[posicao].Value == 6)
                     {
-                        Button.Enabled = false;
+                        buttonDados.Enabled = false;
                     }
 
                     posicao++;
 
-                    if(posicao == barras.Length)
+                    if(posicao == barrasAtivadas)
                     {
                         posicao = 0;
                     }
@@ -92,21 +97,54 @@ namespace Conjuracao
                     break;
 
                 case "REPOR":
-                    CriaLista(dados);
-                    Button.Text = "ROLAR";
+                    Cria_dadosLista(dados);
+                    buttonDados.Text = "ROLAR";
                     break;
             }
         }
 
+        private void buttonJogar_Click(object sender, EventArgs e)
+        {
+            groupBoxMenu.Enabled = false;
+            groupBoxMenu.Hide();
+            groupBoxPartida.Enabled = true;
+            groupBoxPartida.Show();
+        }
+
+        private void buttonAdicionar_Click(object sender, EventArgs e)
+        {
+            barras[barrasAtivadas].Enabled = true;
+            barras[barrasAtivadas].Show();
+            barrasAtivadas++;
+            if(barrasAtivadas == 4)
+            {
+                buttonJogar.Enabled = true;
+            }
+            if (barrasAtivadas == 8)
+            {
+                buttonAdicionar.Enabled = false;
+            }
+        }       
+        
+        private void textBoxEntraJogador_TextChanged(object sender, EventArgs e)
+        {
+            buttonAdicionar.Enabled = true;
+        }
+
         // Outras funções/métodos
 
-        private void CriaLista (Dado[] array)
+        private void Cria_dadosLista (Dado[] array)
         {
             foreach (Dado dado in array)
             {
                 dado.ladoDado.Image = Image.FromFile(System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "/Pics/lado6.png");
                 dadosLista.Add(dado);
             }
+        }
+
+        private void CriaJogadores()
+        {
+            
         }
     }
 }
